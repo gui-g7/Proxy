1948c0abdf41030c3dabe528ff47f6445a5e1cea
Guilherme <142618163+gui-g7@users.noreply.github.com>
2025-01-16 22:34:55 -0300

    Create tcp.rs

---
+++ src/listener/raw_sockets/tcp.rs
@@ -0,0 +1,22 @@
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::Packet;
+use crate::listener::metrics::TrafficMetrics;
+use std::sync::Arc;
+
+pub fn process_raw_tcp_packet(packet: &[u8], metrics: &Arc<TrafficMetrics>) {
+    if let Some(tcp_packet) = TcpPacket::new(packet) {
+        let src_port = tcp_packet.get_source();
+        let dest_port = tcp_packet.get_destination();
+        let payload_size = packet.len() as u64;
+
+        println!(
+            "Raw TCP Packet: Src Port: {}, Dest Port: {}, Size: {} bytes",
+            src_port, dest_port, payload_size
+        );
+
+        // Atualiza métricas
+        metrics.add_data("Raw TCP", payload_size);
+    } else {
+        println!("Failed to parse TCP packet.");
+    }
+}
