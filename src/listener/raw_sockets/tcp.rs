a5cdc9ade0b5ec7c810236d3a290a6b112eea9ee
Guilherme <142618163+gui-g7@users.noreply.github.com>
2025-01-16 23:58:35 -0300

    Update tcp.rs

--- src/listener/raw_sockets/tcp.rs
+++ src/listener/raw_sockets/tcp.rs
@@ -2,6 +2,7 @@ use pnet::packet::tcp::TcpPacket;
 use crate::listener::metrics::TrafficMetrics;
 use std::sync::Arc;
 
+#[allow(unused)]
 pub fn process_raw_tcp_packet(packet: &[u8], metrics: &Arc<TrafficMetrics>) {
     if let Some(tcp_packet) = TcpPacket::new(packet) {
         let src_port = tcp_packet.get_source();
