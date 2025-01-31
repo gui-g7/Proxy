6954ae486e5921ecbad9526d1198daf5450b13b7
Guilherme <142618163+gui-g7@users.noreply.github.com>
2025-01-30 21:18:28 -0300

    Update redis.rs

--- src/data/redis/redis.rs
+++ src/data/redis/redis.rs
@@ -1,1 +1,28 @@
+use redis::{Commands, Connection, RedisResult};
+use std::sync::Mutex;
+
+#[allow(unused)]
+pub struct RedisDB {
+    conn: Mutex<Connection>,
+}
+
+#[allow(unused)]
+impl RedisDB {
+    pub fn new(redis_url: &str) -> RedisResult<Self> {
+        let client = redis::Client::open(redis_url)?;
+        let conn = client.get_connection()?;
+        Ok(RedisDB {
+            conn: Mutex::new(conn),
+        })
+    }
+
+    pub fn store_packet(&self, protocol: &str, data: &str) -> RedisResult<()> {
+        let mut conn = self.conn.lock().unwrap();
+        let key = format!("packet:{}", protocol);
+        
+        let _: i64 = conn.lpush(key, data)?; // Explicitamos o tipo esperado
+        
+        Ok(())
+    }    
+}
 
