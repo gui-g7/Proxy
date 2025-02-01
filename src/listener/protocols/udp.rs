9d705d901266a616453c73ca8ffe0df29804bb4c
Guilherme <142618163+gui-g7@users.noreply.github.com>
2025-01-31 21:34:12 -0300

    Update udp.rs

--- src/listener/protocols/udp.rs
+++ src/listener/protocols/udp.rs
@@ -1,20 +1,35 @@
-use crate::listener::metrics::TrafficMetrics;
-use pnet::packet::udp::UdpPacket;
-use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::Packet;
+use pnet::packet::{ipv4::Ipv4Packet, udp::UdpPacket, Packet};
 use std::sync::Arc;
+use crate::{ api::api::API_CONFIG, listener::metrics::TrafficMetrics };
 
 pub fn process_udp_packet(ipv4_packet: &Ipv4Packet, metrics: &Arc<TrafficMetrics>) {
     if let Some(udp_packet) = UdpPacket::new(ipv4_packet.payload()) {
+        let src_ip = ipv4_packet.get_source();
+        let dst_ip = ipv4_packet.get_destination();
         let src_port = udp_packet.get_source();
         let dst_port = udp_packet.get_destination();
         let payload_size = ipv4_packet.payload().len() as u64;
 
+
+        let src_domains = API_CONFIG.hackertarget_lookup(&src_ip.to_string())
+            .unwrap_or_else(|e| {
+                eprintln!("Erro na consulta de origem {}: {}", src_ip, e);
+                Vec::new()
+            });
+        
+        let dst_domains = API_CONFIG.hackertarget_lookup(&dst_ip.to_string())
+            .unwrap_or_else(|e| {
+                eprintln!("Erro na consulta de destino {}: {}", dst_ip, e);
+                Vec::new()
+            });
+
         println!("=== Pacote UDP ===");
-        println!("Endereço IP de Origem: {}", ipv4_packet.get_source());
-        println!("Endereço IP de Destino: {}", ipv4_packet.get_destination());
+        println!("Endereço IP de Origem: {}", src_ip);
+        println!("Endereço IP de Destino: {}", dst_ip);
         println!("Porta de Origem: {}", src_port);
         println!("Porta de Destino: {}", dst_port);
+        println!("Domínios Associados à Origem: {:?}", src_domains);
+        println!("Domínios Associados ao Destino: {:?}", dst_domains);
         println!("Tamanho do Payload: {} bytes", payload_size);
 
         // Atualiza as métricas
