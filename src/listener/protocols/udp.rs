034e86a8f8278efbd1c66fab795f93afa2bad40f
Guilherme <142618163+gui-g7@users.noreply.github.com>
2025-02-06 18:58:19 -0300

    Update udp.rs

--- src/listener/protocols/udp.rs
+++ src/listener/protocols/udp.rs
@@ -9,19 +9,8 @@ pub fn process_udp_packet(ipv4_packet: &Ipv4Packet, metrics: &Arc<TrafficMetrics
         let src_port = udp_packet.get_source();
         let dst_port = udp_packet.get_destination();
         let payload_size = ipv4_packet.payload().len() as u64;
-
-
-        let src_domains = API_CONFIG.ip_api_lookup(&src_ip.to_string())
-            .unwrap_or_else(|e| {
-                eprintln!("Erro na consulta de origem {}: {}", src_ip, e);
-                Vec::new()
-            });
-        
-        let dst_domains = API_CONFIG.ip_api_lookup(&dst_ip.to_string())
-            .unwrap_or_else(|e| {
-                eprintln!("Erro na consulta de destino {}: {}", dst_ip, e);
-                Vec::new()
-            });
+        let src_domains = API_CONFIG.ip_api_lookup(&src_ip.to_string()).ok().filter(|d| !d.is_empty());
+        let dst_domains = API_CONFIG.ip_api_lookup(&dst_ip.to_string()).ok().filter(|d| !d.is_empty());
 
         println!("=== Pacote UDP ===");
         println!("Endereço IP de Origem: {}", src_ip);
@@ -32,7 +21,9 @@ pub fn process_udp_packet(ipv4_packet: &Ipv4Packet, metrics: &Arc<TrafficMetrics
         println!("Domínios Associados ao Destino: {:?}", dst_domains);
         println!("Tamanho do Payload: {} bytes", payload_size);
 
-        // Atualiza as métricas
+        if let Some(domains) = src_domains {println!("Domínios Associados à Origem: {:?}", domains);}
+        if let Some(domains) = dst_domains {println!("Domínios Associados ao Destino: {:?}", domains);}
+
         metrics.add_data("UDP", payload_size);
     } else {
         println!("Erro ao processar pacote UDP");
